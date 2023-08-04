edfc412837646aeaabc55f99a90ea2abd1247dd1
Netrobe Ubuntu <netrobepy@gmail.com>
2023-08-04 05:09:20 +0100

    update

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 
 puts ARGV[0].scan(/(?<=from:|to:|flags:)[\w|\+|\-|\:]*(?=\])/).join(",")
--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,3 +1,3 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 
 puts ARGV[0].scan(/^h.{1}n$/).join
