f16a8bda36a04db255324828abbb63ec16e37797
Netrobe Ubuntu <netrobepy@gmail.com>
2023-09-23 05:01:18 +0100

    Added my first configuration with puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+file { 'create a file':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+  mode    => '0744'
+}
