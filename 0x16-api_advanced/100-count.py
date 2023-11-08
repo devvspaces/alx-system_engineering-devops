0e1d61b9a5d83f453f69cf2f224c0cfee7daf9c1
Netrobe Ubuntu <netrobepy@gmail.com>
2023-11-08 05:11:41 +0100

    update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,49 +6,57 @@ Count the number of times a keyword appears in the titles of hot posts
 import requests
 
 
-def get_url(subreddit, after=None):
+def get_url(subreddit):
     '''returns the url for a given subreddit'''
-    return 'http://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit, after)
+    return 'http://www.reddit.com/r/{}/hot.json'.format(subreddit)
 
 
-def count_words(subreddit, word_list, found_list=[], after=None):
+headers = {
+    'user-agent': 'Python/requests:APIproject:v1.0.0 (by /u/aaorrico23)'}
+
+
+def count_words(subreddit, word_list, after="", word_dic={}):
     """
-    Return the number of times a keyword appears in the titles of hot posts
-
-    :param subreddit: subreddit to search
-    :type subreddit: str
-    :param word_list: list of words to search for
-    :type word_list: list
-    :param found_list: list of words found, defaults to []
-    :type found_list: list, optional
-    :param after: after, defaults to None
-    :type after: str, optional
+    Get the number of times a keyword appears in the titles of hot posts
     """
-    user_agent = {'User-agent': 'Mozilla/5.0'}
-    posts = requests.get(get_url(subreddit, after), headers=user_agent)
+    if not word_dic:
+        for word in word_list:
+            word_dic[word] = 0
+
     if after is None:
-        word_list = [word.lower() for word in word_list]
+        word_list = [[key, value] for key, value in word_dic.items()]
+        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
+        for w in word_list:
+            if w[1]:
+                print("{}: {}".format(w[0].lower(), w[1]))
+        return None
 
-    if posts.status_code == 200:
-        posts = posts.json()['data']
-        aft = posts['after']
-        posts = posts['children']
-        for post in posts:
-            title = post['data']['title'].lower()
-            for word in title.split(' '):
-                if word in word_list:
-                    found_list.append(word)
-        if aft is not None:
-            count_words(subreddit, word_list, found_list, aft)
-        else:
-            result = {}
-            for word in found_list:
-                if word.lower() in result.keys():
-                    result[word.lower()] += 1
-                else:
-                    result[word.lower()] = 1
-            for key, value in sorted(result.items(), key=lambda item: item[1],
-                                     reverse=True):
-                print('{}: {}'.format(key, value))
-    else:
-        return
+    url = get_url(subreddit)
+
+    params = {
+        'limit': 100,
+        'after': after
+    }
+
+    r = requests.get(url, headers=headers, params=params,
+                     allow_redirects=False)
+
+    if r.status_code != 200:
+        return None
+
+    try:
+        js = r.json()
+        data = js.get("data")
+        after = data.get("after")
+        children = data.get("children")
+        for child in children:
+            post = child.get("data")
+            title = post.get("title")
+            lower = [s.lower() for s in title.split(' ')]
+
+            for w in word_list:
+                word_dic[w] += lower.count(w.lower())
+    except Exception:
+        return None
+
+    count_words(subreddit, word_list, after, word_dic)
