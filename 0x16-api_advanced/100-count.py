9be39ad3d1a75996b1bd110663e3527483539573
Netrobe Ubuntu <netrobepy@gmail.com>
2023-11-08 05:14:29 +0100

    update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-"""
-Count the number of times a keyword appears in the titles of hot posts
-"""
+""" Count it! """
+from requests import get
 
-import requests
-
-
-def get_url(subreddit):
-    '''returns the url for a given subreddit'''
-    return 'http://www.reddit.com/r/{}/hot.json'.format(subreddit)
-
-
-headers = {
-    'user-agent': 'Python/requests:APIproject:v1.0.0 (by /u/aaorrico23)'}
+REDDIT = "https://www.reddit.com/"
+HEADERS = {'user-agent': 'my-app/0.0.1'}
 
 
 def count_words(subreddit, word_list, after="", word_dic={}):
     """
-    Get the number of times a keyword appears in the titles of hot posts
+    Returns a list containing the titles of all hot articles for a
+    given subreddit. If no results are found for the given subreddit,
+    the function should return None.
     """
     if not word_dic:
         for word in word_list:
@@ -31,21 +24,26 @@ def count_words(subreddit, word_list, after="", word_dic={}):
                 print("{}: {}".format(w[0].lower(), w[1]))
         return None
 
-    url = get_url(subreddit)
+    url = REDDIT + "r/{}/hot/.json".format(subreddit)
 
     params = {
         'limit': 100,
         'after': after
     }
 
-    r = requests.get(url, headers=headers, params=params,
-                     allow_redirects=False)
+    r = get(url, headers=HEADERS, params=params, allow_redirects=False)
 
     if r.status_code != 200:
         return None
 
     try:
         js = r.json()
+
+    except ValueError:
+        return None
+
+    try:
+
         data = js.get("data")
         after = data.get("after")
         children = data.get("children")
@@ -56,7 +54,8 @@ def count_words(subreddit, word_list, after="", word_dic={}):
 
             for w in word_list:
                 word_dic[w] += lower.count(w.lower())
-    except Exception:
+
+    except:
         return None
 
     count_words(subreddit, word_list, after, word_dic)
